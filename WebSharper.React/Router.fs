85dd4dd7025283720bc8af5e4f24ad0346b63b8e
Loic Denuziere <loic@denuziere.net>
2018-08-29 15:16:18 +0200

    Create empty gh-pages

--- WebSharper.React/Router.fs
+++
@@ -1,2 +0,0 @@
-namespace WebSharper.React
-
--- tools/publish-site.ps1
+++
@@ -1,8 +0,0 @@
-# creates build/html
-rm -r build -errorAction ignore
-$d = mkdir build
-$d = mkdir build/html
-cp -r WebSharper.React.Tests/Content build/html/
-cp -r WebSharper.React.Tests/*.jpg build/html/
-cp -r WebSharper.React.Tests/*.css build/html/
-cp -r WebSharper.React.Tests/*.html build/html/
