ef0834ea2f5b30d0b23ff32bf29a84987b0f9fb6
jooseppi12 <urijozsef@europe.com>
2021-09-02 15:07:06 +0200

    #8 - Use WS5, Prepare for GHA stack

--- build.fsx
+++ build.fsx
@@ -20,6 +20,16 @@ nuget Paket.Core //"
 #endif
 
 #load "paket-files/wsbuild/github.com/dotnet-websharper/build-script/WebSharper.Fake.fsx"
+#r "System.Xml.Linq"
+
+// Only reference these packages from editors/non fake-cli tools
+#if !FAKE
+    // To have proper language service in the editor
+    #r "netstandard"
+    // To help FAKE related IntelliSense in editor
+    #load "./.fake/build.fsx/intellisense_lazy.fsx"
+#endif
+
 open Fake.Core
 open Fake.Core.TargetOperators
 open Fake.DotNet
@@ -28,7 +38,7 @@ open Fake.IO.FileSystemOperators
 open WebSharper.Fake
 
 let targets =
-    WSTargets.Default (fun () -> GetSemVerOf "WebSharper" |> ComputeVersion)
+    LazyVersionFrom "WebSharper" |> WSTargets.Default
     |> MakeTargets
 
 Target.create "AppVeyor" ignore
