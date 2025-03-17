ed17e0432cf6c5cfc29c3f750f55270cb4f762b2
András Jankó <andras.janko@gmail.com>
2025-03-17 17:23:26 +0100

    add esbuild to tests

---
+++ WebSharper.React.Tests/esbuild.config.mjs
@@ -0,0 +1,14 @@
+﻿import { build } from 'esbuild'
+
+var options =
+{
+  entryPoints: ['./build/WebSharper.React.Tests.js'],
+  bundle: true,
+  minify: true,
+  format: 'iife',
+  outfile: './Content/WebSharper.React.Tests.min.js',
+  globalName: 'wsbundle'
+};
+
+build(options);
+
